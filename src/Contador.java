9669f0d44c2d002140fc1d70787f89b6b0f104e0
Marcellyz <marcelly.eduarda4545@gmail.com>
2024-05-05 19:29:26 -0300

    criando o método contar

--- src/Contador.java
+++ src/Contador.java
@@ -1,5 +1,32 @@
+
+import java.util.Scanner;
+
 public class Contador {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro:");
+        int parametroUm = terminal.nextInt();
+
+        System.out.println("Digite o segundo parâmetro:");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+
+        } catch ( Exception RuntimeException) {
+            System.out.println("Você digitou um número fora da contagem entre" + parametroUm + " e " + parametroDois + RuntimeException.getMessage());
+        }
+    }
+
+    private static void contar(int parametroUm, int parametroDois) throws ParametrosIvanlidosExpection {
+        if (parametroUm > parametroDois) {
+           throw new ParametrosIvanlidosExpection();
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for(int i = contagem; i <= parametroDois;i++){
+            System.out.println("Imprimindo o numero" + i);
+        };
+
     }
 }
